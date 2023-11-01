a9dbc793d21ea3e805eb080c03fc6bf3064eab99
Tianwei Liu <TLiu25@slb.com>
2023-11-01 14:32:14 +0800

    feat: should return fizz when countoff given number multiple of 3

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -4,6 +4,10 @@
     {
         public string DoGame(int number)
         {
+            if (number % 3 == 0)
+            {
+                return "fizz";
+            }
             return number.ToString();
         }
 }
