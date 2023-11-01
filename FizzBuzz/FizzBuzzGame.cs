061087d8c5846789eb7a265756859bd1d028fe4b
Tianwei Liu <TLiu25@slb.com>
2023-11-01 14:34:42 +0800

    feat: shoule return buzz when countoff given number being multiple of 5

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -8,6 +8,10 @@
             {
                 return "fizz";
             }
+            else if (number % 5 == 0)
+            {
+                return "buzz";
+            }
             return number.ToString();
         }
 }
